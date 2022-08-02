5afecb555d264c3312d6327f457a147cd649361c
ajoy biswas <ajoypust@gmail.comm>
2022-08-03 02:48:25 +0600

    upload json token add

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import auth from '../../Firebase/Firebase.init';
-import { useAuthState, useSignInWithGoogle} from 'react-firebase-hooks/auth';
+import { useAuthState, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
 const Login = () => {
     const [signInWithGoogle, googleuser, googleloading, googleerror] = useSignInWithGoogle(auth);
-    
-    const[user]=useAuthState(auth)
+
+    const [user] = useAuthState(auth)
     const navigate = useNavigate();
     const location = useLocation();
     const from = location.state?.from?.pathname || "/";
-    if(user){
-        navigate(from, { replace: true });
+    if (user) {
+        // console.log(user.email)
+        const url='http://localhost:5000/login'
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+                email:user.email
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((res) => res.json())
+            .then((data) =>{
+                localStorage.setItem('access_token',data.token)
+                navigate(from, { replace: true });
+            });
+       
     }
     return (
         <div className='text-center mt-3'>
             <h1> Login</h1>
-            <button onClick={()=>signInWithGoogle()} type="button" className="btn btn-success">Google Login</button>
+            <button onClick={() => signInWithGoogle()} type="button" className="btn btn-success">Google Login</button>
         </div>
     );
 };
--- src/Component/UploadProduct/UploadProduct.js
+++ src/Component/UploadProduct/UploadProduct.js
@@ -1,13 +1,31 @@
 
 import React from 'react';
 import Form from 'react-bootstrap/Form';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../Firebase/Firebase.init';
 
 const UploadProduct = () => {
-    const handleSubmit=(event)=>{
+    const[user]=useAuthState(auth)
+    const handleSubmit = (event) => {
         event.preventDefault()
-        const name=event.target.name.value;
-        const price=event.target.price.value;
-        console.log(name,price)
+        const name = event.target.name.value;
+        const price = event.target.price.value;
+        console.log(name, price)
+        const url='http://localhost:5000/upladepd'
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+                name, price
+            }),
+            headers: {
+                "authorization":`
+               ${user.email} ${localStorage.getItem('access_token')}`,
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then(res => res.json())
+            .then(data => console.log(data));
+         event.target.reset()   
     }
     return (
         <div className='mx-auto w-50'>
@@ -16,16 +34,16 @@ const UploadProduct = () => {
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Product Name</Form.Label>
                     <Form.Control type="text" placeholder="Name" name="name" />
-                    
+
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Price</Form.Label>
                     <Form.Control type="text" placeholder="Product price" name='price' />
                 </Form.Group>
-                
+
                 <input className='btn btn-primary' variant="primary" type="submit" value='Upload Product'>
-                    
+
                 </input>
             </Form>
         </div>
