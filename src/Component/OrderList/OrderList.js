c4e97908ee8955c17f6e1195f33fa9cbf111a538
ajoy biswas <ajoypust@gmail.comm>
2022-08-03 10:05:42 +0600

    add products

--- src/Component/OrderList/OrderList.js
+++ src/Component/OrderList/OrderList.js
@@ -1,19 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const OrderList = () => {
-    const[products,setProducts]=useState([])
-    console.log(products)
-    useEffect(()=>{
-        fetch('http://localhost:5000/products')
-        .then(res=>res.json())
-        .then(data=>setProducts(data))
-    },[])
+  
     return (
         <div className='text-center'>
-            <h1 className='text-center'>this is order list {products.length}</h1>
-            {
-                products.map(product=><li>{product.name}</li>)
-            }
+            <h1 className='text-center'>this is order list</h1>
+            
         </div>
     );
 };
--- src/Component/Products/Products.js
+++ src/Component/Products/Products.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Products = () => {
+    const[products,setProducts]=useState([])
+    console.log(products)
+    useEffect(()=>{
+        fetch('http://localhost:5000/products')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    },[])
     return (
-        <div>
+        <div className='text-center'>
             <h1>this is products</h1>
+            {
+                products.map(product=><li>{product.name}</li>)
+            }
         </div>
     );
 };
