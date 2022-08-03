569afd571714196906b21a953c259051204ca745
ajoy biswas <ajoypust@gmail.comm>
2022-08-03 10:02:29 +0600

    product list

--- src/Component/OrderList/OrderList.js
+++ src/Component/OrderList/OrderList.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const OrderList = () => {
+    const[products,setProducts]=useState([])
+    console.log(products)
+    useEffect(()=>{
+        fetch('http://localhost:5000/products')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    },[])
     return (
-        <div>
-            <h1>this is order list</h1>
+        <div className='text-center'>
+            <h1 className='text-center'>this is order list {products.length}</h1>
+            {
+                products.map(product=><li>{product.name}</li>)
+            }
         </div>
     );
 };
